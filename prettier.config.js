d9d46d2c022b344909176052e706df2b94b1dd32
AlisaLiCn <alisali1993@163.com>
2025-04-22 14:30:42 +0800

    feat: init files

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+    printWidth: 120,
+    tabWidth: 2,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'none',
+    quoteProps: 'as-needed',
+    arrowParens: 'avoid',
+    endOfLine: 'auto'
+  }
+  
