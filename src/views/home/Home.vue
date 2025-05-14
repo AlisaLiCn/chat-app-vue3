c36baaa03a79cac24a6515a05c6ede4aed5e7c16
AlisaLiCn <alisali1993@163.com>
2025-05-14 16:43:35 +0800

    feat: ts、ignore config

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-
+import type { DrawerProps } from 'element-plus'
 
 defineOptions({
   name: 'Home'
 })
 
+const drawer = ref(false)
+const direction = ref<DrawerProps['direction']>('ltr')
 </script>
 
 <template>
-  home
+  <el-button type="primary" style="margin-left: 16px" @click="drawer = true"> open </el-button>
+  <el-drawer v-model="drawer" title="I am the title" :direction="direction" :modal="false">
+    <span>Hi, there!</span>
+  </el-drawer>
 </template>
