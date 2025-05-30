0ee657f43c3c6dfad373cb9dbd4db1ac5e53817f
AlisaLiCn <alisali1993@163.com>
2025-05-30 11:05:04 +0800

    feat: add conversation list demo

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { BubbleList, Sender } from 'vue-element-plus-x'
+import type { ConversationItem } from 'vue-element-plus-x/types/Conversations'
+import { Conversations, BubbleList, Sender } from 'vue-element-plus-x'
 import { streamChat } from '@/services/chat'
 
 defineOptions({
@@ -17,6 +18,24 @@ const userInput = ref('')
 const messages = ref<MessageItem[]>([])
 const currentAnswer = ref('')
 const isLoading = ref(false)
+const activeConversation = ref()
+const timeBasedItems = ref<ConversationItem<{ id: number; label: string }>[]>([
+  {
+    id: 1,
+    label: '今天的会话111',
+    group: 'today'
+  },
+  {
+    id: 2,
+    group: 'today',
+    label: '今天的会话2'
+  },
+  {
+    id: 3,
+    group: 'yesterday',
+    label: '昨天的会话1'
+  }
+])
 
 let abortController: AbortController | null = null
 
@@ -28,6 +47,10 @@ const msgList = computed(() => {
   if (!currentAnswer.value) return messages.value
   const currentMsg = { role: 'assistant', content: currentAnswer.value, isMarkdown: true }
   return [...messages.value, currentMsg]
+})
+
+watch(activeConversation.value, newVal => {
+  console.log('new conversaion', newVal)
 })
 
 // 组件卸载时取消未完成的请求
@@ -68,32 +91,48 @@ const sendMessage = async () => {
 }
 
 const handleCancel = () => {}
+
+function handleConversationChange(item: ConversationItem<{ id: string; label: string }>) {
+  console.log('conversation changed', item)
+}
 </script>
 
 <template>
-  <div class="h-screen flex flex-col flex-1">
-    <div class="h-full flex flex-col justify-center items-center">
-      <div class="h-[56px]"></div>
-      <div class="chat-content flex-1 flex-grow h-full">
-        <BubbleList :list="msgList" :max-height="bubbleMaxHeight" :btnLoading="isLoading" />
-      </div>
-      <div class="chat-content pb-[30px]">
-        <Sender
-          ref="senderRef"
-          v-model="userInput"
-          :loading="isLoading"
-          clearable
-          @submit="sendMessage"
-          @cancel="handleCancel"
-        />
+  <div class="flex flex-1">
+    <div class="flex flex-col gap-[12px] h-full">
+      <Conversations
+        v-model:active="activeConversation"
+        :items="timeBasedItems"
+        :label-max-width="200"
+        :show-tooltip="true"
+        row-key="id"
+        @change="handleConversationChange"
+      />
+    </div>
+    <div class="w-full h-screen flex flex-col flex-1">
+      <div class="w-full h-full flex flex-col justify-center items-center">
+        <div class="h-[56px]"></div>
+        <div class="chat-content flex-1 flex-grow h-full">
+          <BubbleList :list="msgList" :max-height="bubbleMaxHeight" :btnLoading="isLoading" />
+        </div>
+        <div class="chat-content pb-[30px]">
+          <Sender
+            ref="senderRef"
+            v-model="userInput"
+            :loading="isLoading"
+            clearable
+            @submit="sendMessage"
+            @cancel="handleCancel"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style>
+<style lang="less" scoped>
 .chat-content {
-  --padding-side: calc(50vw - 401px);
+  --padding-side: calc(50% - 401px);
 
   width: 100%;
   padding: 0 var(--padding-side);
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig(({ mode }) => {
   const root: string = process.cwd()
@@ -11,21 +11,16 @@ export default defineConfig(({ mode }) => {
   const devBaseUrl = env.VITE_APP_SERVER_ADDRESS
 
   return {
-      plugins: [
+    plugins: [
       vue(),
       AutoImport({
         imports: ['vue', 'vue-router'],
         dts: 'src/types/auto-imports.d.ts',
-        resolvers: [
-          ElementPlusResolver(),
-        ],
-
+        resolvers: [ElementPlusResolver()]
       }),
       Components({
         dts: 'src/types/components.d.ts',
-        resolvers: [
-          ElementPlusResolver(),
-        ],
+        resolvers: [ElementPlusResolver()]
       })
     ],
     resolve: {
@@ -40,7 +35,7 @@ export default defineConfig(({ mode }) => {
         '^/api': {
           target: devBaseUrl,
           changeOrigin: true
-        },
+        }
       }
     }
   }
